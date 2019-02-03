fcde93395f88d6d6d78e2de11b5b1737e4ffb8ba
Guy Korland <gkorland@gmail.com>
2019-02-03 12:01:25 +0200

    fix reference to Lable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Record = require("./src/record"),
 	RedisGraph = require("./src/redisGraph"),
 	ResultSet = require("./src/resultSet"),
 	Statistics = require("./src/statistics"),
-	Label = require("./label");
+	Label = require("./src/label");
 
 module.exports = {
 	Record: Record,
