a14e9063157bfea84af0a52d016c8bcaaee88a2c
Himank Jog <himank.juit@gmail.com>
2019-03-28 19:02:15 +0530

    Added the Edge class to the redisGraph (#3)
    * Created the Edge class in a separate file
    
    * Added the Edge class to the graph

--- src/redisGraph.js
+++ src/redisGraph.js
@@ -1,6 +1,7 @@
 const redis = require("redis"),
 	util = require("util"),
-	ResultSet = require("./resultSet");
+	ResultSet = require("./resultSet"),
+	assert = require('assert');
 
 /**
  * RedisGraph Node
@@ -46,34 +47,101 @@ class Node {
 	   * 
 	   * @return Node details in a string
 	   */
-	  toString () {
+	toString () {
 		  let nodeString = '(';
 	  
 		  // Adding the alias to the node string
 		  nodeString += (this.alias || '');
 	  
 		  // Adding the label to the node string
-		  if (this.label !== null) {
-			  nodeString += ':' + this.label;
-		  }
-		  
-		  if (this.properties && this.properties !== {}){
-			// Formating properties to add to the string
-			let properties = JSON.stringify(this.properties);
-
-			// Removing the double quotes around the keys
-			properties = properties.replace(/\"(\w*)\":/g, "$1:");
-
-			// Adding the properties to the node string
-			// Giving the space = 2 by default
-			nodeString += ' ' + properties;
-		  }
-
-		  nodeString += ')';
-	  
-		  return nodeString;
+		if (this.label !== null) {
+			nodeString += ':' + this.label;
 		}
-  }
+		
+		if (this.properties && this.properties !== {}){
+		// Formating properties to add to the string
+		let properties = JSON.stringify(this.properties);
+
+		// Removing the double quotes around the keys
+		properties = properties.replace(/\"(\w*)\":/g, "$1:");
+
+		// Adding the properties to the node string
+		// Giving the space = 2 by default
+		nodeString += ' ' + properties;
+		}
+
+		nodeString += ')';
+	
+		return nodeString;
+	}
+}
+
+/**
+ * RedisGraph Edge
+ */
+class Edge {
+	/**
+	 * Creates edge with provided source node, relation, destination node
+	 * and properties
+	 * @param srcNode The source node of the edge 
+	 * @param relation The relation between the source node and the destination node
+	 * @param destNode The destination node of the edge
+	 * @param properties The properties of the relation/edge
+	 */
+    constructor (srcNode, relation, destNode, properties = null) {
+
+		/**
+		 * The source node cannot be null and has to be of Node Type
+		 * Similaryly, the destination node cannot be null and has to be Edge Type
+		 */
+        assert.notEqual(srcNode, null);
+        assert.equal(srcNode instanceof Node, true);
+        assert.notEqual(destNode, null);
+        assert.equal(destNode instanceof Node, true);
+
+        this.srcNode = srcNode;
+        this.relation = relation;
+        this.destNode = destNode;
+        this.properties = properties;
+    }
+
+	/**
+	   * TO get the edge in a string format.
+	   * So that it can be added to the graph query
+	   * 
+	   * @return Edge details in a string
+	   */
+    toString () {
+
+		// The source node (sourceNodeAlias)
+        let edgeString = '(' + this.srcNode.getAlias() + ')';
+
+		// The relation (sourceNodeAlias)-[:relation]
+        edgeString += '-[';
+
+        if ( this.relation !== null) {
+            edgeString += ': ' + this.relation;
+        }
+
+        if ( this.properties != null || this.properties != {}) {
+            // Formating properties to add to the string
+		  let properties = JSON.stringify(this.properties);
+  
+		  // Removing the double quotes around the keys
+          properties = properties.replace(/\"(\w*)\":/g, "$1:");
+          
+          edgeString = ' ' + properties;
+        }
+
+        edgeString += ']->';
+
+		// The destination node (sourceNodeAlias)-[:relation]->(destinationNodeAlias)
+        edgeString += '(' + this.destNode.getAlias() + ')';
+
+        return edgeString;
+    }
+}
+
 /**
  * RedisGraph client
  */
