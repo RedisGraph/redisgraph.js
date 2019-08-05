d7a3e83bb1dc454343dfbf497687440707f1b1c0
DvirDukhan <dvir@redislabs.com>
2019-08-05 09:24:34 +0300

    fix readme.md, example, null value to string. closes #20, closes #18. might close #19

--- src/record.js
+++ src/record.js
@@ -2,47 +2,55 @@
  * Hold a query record
  */
 class Record {
-	constructor(header, values) {
-		this._header = header;
-		this._values = values;
-	}
+    constructor(header, values) {
+        this._header = header;
+        this._values = values;
+    }
 
-	get(key) {
-		let index = key;
-		if (typeof key === "string") {
-			index = this._header.indexOf(key);
-		}
-		return this._values[index];
-	}
-	
-	getString(key) {
-		let index = key;
-		if (typeof key === "string") {
-			index = this._header.indexOf(key);
-		}
-		
-		if (this._values[index]) {
-			return this._values[index].toString();
-		}
-		
-		return null;
-	}
+    get(key) {
+        let index = key;
+        if (typeof key === "string") {
+            index = this._header.indexOf(key);
+        }
+        return this._values[index];
+    }
 
-	keys() {
-		return this._header;
-	}
+    getString(key) {
+        let index = key;
+        if (typeof key === "string") {
+            index = this._header.indexOf(key);
+        }
 
-	values() {
-		return this._values;
-	}
+        if (this._values[index]) {
+            return this._values[index].toString();
+        }
 
-	containsKey(key) {
-		return this._header.includes(key);
-	}
+        return null;
+    }
 
-	size() {
-		return this._header.length;
-	}
+    getString(key) {
+        let index = key;
+        if (typeof key === "string") {
+            index = this._header.indexOf(key);
+        }
+        return this._values[index] ? this._values[index].toString() : null;
+    }
+
+    keys() {
+        return this._header;
+    }
+
+    values() {
+        return this._values;
+    }
+
+    containsKey(key) {
+        return this._header.includes(key);
+    }
+
+    size() {
+        return this._header.length;
+    }
 }
 
 module.exports = Record;
